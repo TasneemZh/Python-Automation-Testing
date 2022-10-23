afb8fbeda784b9c189268300f5db9c5df8f5cac0
Tasneem Zahdeh <tasneemzh.1998@gmail.com>
2022-10-23 23:35:44 +0300

    Add results of allure into gitignore

--- manage-csv/classes/csv/ManageCsv.py
+++ manage-csv/classes/csv/ManageCsv.py
@@ -1,20 +1,26 @@
 import csv
 
-class ManageCsv:
-        @staticmethod
-        def read_csv():
-            with open('test.csv', 'rt') as file:
-                reader = csv.reader(file)
-                next(reader)
-                print("Reading CSV File:-")
-                for row in reader:
-                    print(row)
 
-        @staticmethod
-        def write_csv():
-            with open('test.csv', 'w', newline='') as file:
-                writer = csv.writer(file)
-                writer.writerows([["rowNum", "fieldCol1", "fieldCol2"], ["row1", "field1", "field2"], ["row2", "field1", "field2"]])
+class ManageCsv:
+    @staticmethod
+    def read_csv():
+        with open('test.csv', 'rt') as file:
+            reader = csv.reader(file)
+            next(reader)
+            print("Reading CSV File:-")
+            for row in reader:
+                print(row)
+
+    @staticmethod
+    def write_csv():
+        with open('test.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows([
+                ["rowNum", "fieldCol1", "fieldCol2"],
+                ["row1", "field1", "field2"],
+                ["row2", "field1", "field2"]
+            ])
+
 
 ManageCsv.write_csv()
 ManageCsv.read_csv()
